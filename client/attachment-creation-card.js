6519c167a51da859cb14b346122b4c5299c976cb
Murky <moosung.gil@hatiolab.com>
2019-10-08 10:26:55 +0900

    file-selector and image-upload-previewer

--- client/attachment-creation-card.js
+++ client/attachment-creation-card.js
@@ -1,13 +1,17 @@
 import { LitElement, html, css } from 'lit-element'
 import { i18next, localize } from '@things-factory/i18n-base'
 import { AttachmentImporter } from './attachment-importer'
+import './components/file-selector'
+import '@things-factory/image-uploader-ui/client/image-upload-previewer'
 
 export class AttachmentCreationCard extends localize(i18next)(LitElement) {
   static get properties() {
     return {
       /* default category id */
       defaultCategory: String,
-      categories: Array
+      categories: Array,
+      _files: Array,
+      _currentCategory: String
     }
   }
 
@@ -78,20 +82,26 @@ export class AttachmentCreationCard extends localize(i18next)(LitElement) {
 
         [back] form {
           padding: var(--card-list-create-form-padding);
-          display: flex;
-          flex-flow: row wrap;
+          width: 100%;
+          height: 100%;
+          box-sizing: border-box;
+          display: grid;
+          grid-template-columns: repeat(10, 1fr);
+          grid-template-rows: min-content min-content 1fr min-content;
+          grid-row-gap: 7px;
+          justify-content: center;
+          align-items: center;
         }
 
         [back] form label {
-          flex: 1 1 25%;
+          grid-column: span 4;
           font: var(--card-list-create-label-font);
           color: var(--card-list-create-label-color);
         }
 
         [back] form input,
         [back] form select {
-          flex: 1 1 60%;
-          width: 10px;
+          grid-column: span 6;
           background-color: #fff;
           border: var(--card-list-create-input-border);
           border-radius: var(--card-list-create-input-border-radius);
@@ -100,17 +110,38 @@ export class AttachmentCreationCard extends localize(i18next)(LitElement) {
           color: var(--card-list-create-input-color);
         }
 
-        form * {
-          margin: var(--card-list-create-margin);
+        file-selector {
+          grid-column: span 6;
+          font: var(--card-list-create-input-font);
+          border: none;
+          box-sizing: border-box;
+          padding: 0;
         }
 
-        input[type='submit'] {
+        #thumbnail-area {
+          grid-column: span 10;
+          align-self: stretch;
+          text-align: center;
+          overflow: hidden;
+          overflow-x: auto;
+          white-space: nowrap;
+        }
+
+        image-upload-previewer {
+          display: inline-block;
+          height: 100%;
+        }
+
+        [back] input[type='submit'] {
           background-color: var(--button-background-color) !important;
           margin: var(--button-margin);
           font: var(--button-font);
           color: var(--button-color) !important;
           border-radius: var(--button-radius);
           border: var(--button-border);
+          grid-column: span 10;
+          grid-row: auto / -1;
+          margin: 0;
         }
       `
     ]
@@ -127,7 +158,13 @@ export class AttachmentCreationCard extends localize(i18next)(LitElement) {
       <div @click=${e => this.onClickFlip(e)} back>
         <form @submit=${e => this.onClickSubmit(e)}>
           <label>${i18next.t('label.category')}</label>
-          <select .value=${this.defaultCategory} name="category">
+          <select
+            .value=${this.defaultCategory}
+            name="category"
+            @change=${e => {
+              this._currentCategory = e.currentTarget.value
+            }}
+          >
             <option value="">--${i18next.t('text.please choose a category')}--</option>
             ${categories.map(
               category => html`
@@ -137,12 +174,40 @@ export class AttachmentCreationCard extends localize(i18next)(LitElement) {
           </select>
 
           <label>${i18next.t('label.file')}</label>
-          <input type="file" name="file" multiple />
+          <file-selector
+            name="file"
+            label="${i18next.t('label.select file')}"
+            accept="${this._currentCategory || '*'}/*"
+            multiple
+            @file-change=${e => {
+              this._files = Array.from(e.detail.files)
+            }}
+          ></file-selector>
+
+          <div id="thumbnail-area">
+            ${this._files.map(
+              file => html`
+                <image-upload-previewer .file=${file}></image-upload-previewer>
+              `
+            )}
+          </div>
+
+          <!-- ${/^(video|image)/.test((this._files || {}).type)
+            ? html`
+                <image-upload-previewer .file=${this._files}></image-upload-previewer>
+              `
+            : html``} -->
 
           <input type="submit" value=${i18next.t('button.create')} />
         </form>
       </div>
     `
+  }
+
+  constructor() {
+    super()
+
+    this._files = []
   }
 
   firstUpdated() {
@@ -167,7 +232,7 @@ export class AttachmentCreationCard extends localize(i18next)(LitElement) {
     var form = e.target
 
     var category = form.elements['category'].value
-    var files = form.elements['file'].files
+    var files = this._files
 
     this.dispatchEvent(
       new CustomEvent('create-attachment', {
@@ -183,6 +248,7 @@ export class AttachmentCreationCard extends localize(i18next)(LitElement) {
     var form = this.shadowRoot.querySelector('form')
     if (form) {
       form.reset()
+      this._files = []
     }
 
     this.classList.remove('flipped')
---
+++ client/components/file-selector.js
@@ -0,0 +1,158 @@
+import '@material/mwc-icon'
+import '@material/mwc-icon-button'
+import { css, html, LitElement } from 'lit-element'
+
+export class FileSelector extends LitElement {
+  static get styles() {
+    return [
+      css`
+        :host {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          position: relative;
+          overflow: hidden;
+        }
+
+        #input-box {
+          display: flex;
+          width: 100%;
+          height: 100%;
+        }
+
+        input[type='file'] {
+          position: absolute;
+          width: 0px;
+          height: 0px;
+          padding: 0;
+          margin: -1px;
+          overflow: hidden;
+          clip: rect(0, 0, 0, 0);
+          border: 0;
+        }
+
+        label {
+          padding: unset;
+          width: 100%;
+          height: 100%;
+          box-sizing: border-box;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          color: var(--file-selector-color, #999);
+          font-size: inherit;
+          line-height: normal;
+          vertical-align: middle;
+          background-color: var(--file-selector-bg-color, #fdfdfd);
+          cursor: pointer;
+          border: var(--file-selector-border, 1px solid #ebebeb);
+          border-radius: var(--file-selector-border-radius, 0.25em);
+        }
+
+        /* named upload */
+        .upload-name {
+          flex: 1;
+          padding: 0.5em 0.75em; /* label의 패딩값과 일치 */
+          font-size: inherit;
+          font-family: inherit;
+          line-height: normal;
+          vertical-align: middle;
+          background-color: #f5f5f5;
+          border: 1px solid #ebebeb;
+          border-bottom-color: #e2e2e2;
+          border-radius: 0.25em;
+          -webkit-appearance: none; /* 네이티브 외형 감추기 */
+          -moz-appearance: none;
+          appearance: none;
+        }
+      `
+    ]
+  }
+
+  static get properties() {
+    return {
+      label: String,
+      accept: String,
+      showFilename: {
+        type: Boolean,
+        attribute: 'show-filename'
+      },
+      multiple: {
+        type: Boolean,
+        attribute: 'multiple'
+      },
+      _files: Array
+    }
+  }
+
+  constructor() {
+    super()
+
+    this._files = []
+    this.label = 'select file'
+    this.multiple = false
+  }
+
+  render() {
+    return html`
+      <div id="input-box">
+        ${this.showFilename
+          ? html`
+              <input class="upload-name" value="${this._files.map(f => f.name).join(', ') || this.label}" disabled />
+            `
+          : html``}
+        <label for="input-file">${this.label}</label>
+        <input
+          id="input-file"
+          type="file"
+          accept="${this.accept}"
+          class="upload-hidden"
+          ?multiple=${this.multiple}
+          hidden
+          @change=${e => {
+            this.dispatchEvent(
+              new CustomEvent('file-change', {
+                bubbles: true,
+                composed: true,
+                detail: {
+                  files: e.currentTarget.files
+                }
+              })
+            )
+          }}
+        />
+      </div>
+    `
+  }
+
+  get fileInput() {
+    return this.renderRoot.querySelector('#input-file')
+  }
+
+  onImageFileChanged(e) {
+    this._files = [...this._files, ...Array.from(e.currentTarget.files)]
+  }
+
+  async _onUploadButtonClick(e) {
+    var response = await this.createAttachments('', this._files)
+    var { createAttachments } = response.data
+
+    if (createAttachments.length == 0) return
+
+    this._files = []
+    this.fileInput.value = ''
+
+    this.dispatchEvent(
+      new CustomEvent('image-upload-succeeded', {
+        bubbles: true,
+        composed: true,
+        detail: {
+          files: createAttachments
+        }
+      })
+    )
+  }
+}
+
+window.customElements.define('file-selector', FileSelector)
